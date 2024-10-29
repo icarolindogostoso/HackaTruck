ad8c2bc4466c2ed9490e7ba18ee565562c7c24c0
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2024-10-29 16:55:24 -0300

    Update Desafio_05.swift

--- Aula_01/Aula_01/Desafio_05.swift
+++ Aula_01/Aula_01/Desafio_05.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct Desafio_05: View {
-    @State private var altura: Double = 0
-    @State private var peso: Double = 0
+    @State private var altura: String = ""
+    @State private var alturaNumero: Double = 0
+    @State private var peso: String = ""
+    @State private var pesoNumero: Double = 0
     @State private var texto: String = "IMC"
     @State private var cor: String = "branco"
     @State var varial: Double = 0.0
@@ -19,9 +21,9 @@ struct Desafio_05: View {
             VStack{
                 Text("Calculadora de IMC").font(.largeTitle)
                 
-                TextField("Digite uma altura", value: $altura, format: .number).keyboardType(.decimalPad).textContentType(.oneTimeCode).padding().background(Color.white).cornerRadius(10).multilineTextAlignment(.center)
+                TextField("Digite uma altura", text: $altura).keyboardType(.decimalPad).textContentType(.oneTimeCode).padding().background(Color.white).cornerRadius(10).multilineTextAlignment(.center)
                 
-                TextField("Digite um peso", value: $peso, format: .number).keyboardType(.decimalPad).textContentType(.oneTimeCode).padding().background(Color.white).cornerRadius(10).multilineTextAlignment(.center)
+                TextField("Digite um peso", text: $peso).keyboardType(.decimalPad).textContentType(.oneTimeCode).padding().background(Color.white).cornerRadius(10).multilineTextAlignment(.center)
                 
                 Button(action: {coiso()}, label: {Text("Calcular").font(.headline).bold().foregroundStyle(.white).padding().background(Capsule())})
                 
@@ -37,7 +39,9 @@ struct Desafio_05: View {
     }
     
     func coiso(){
-         varial = peso / pow(altura,2)
+        pesoNumero = (peso as NSString).doubleValue
+        alturaNumero = (altura as NSString).doubleValue
+         varial = pesoNumero / pow(alturaNumero,2)
         if (varial < 18.5){
             texto = "Baixo peso"
             cor = "baixopeso"
