a236a43e038966f62e302e741f2a349396317893
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2024-11-01 14:12:18 -0300

    Update merda.swift

--- project3/project3/merda.swift
+++ project3/project3/merda.swift
@@ -7,9 +7,44 @@
 
 import SwiftUI
 
+struct Songue : Hashable{
+    var name : String
+    var artist : String
+    var capa : String
+}
+
 struct merda: View {
+    let url = URL(string: "https://i.scdn.co/image/ab67616d0000b273708ed519ab2ea26c8ba69a19")
+    let musica = Song(name: "merda", artist: "merda", capa: "https://i.scdn.co/image/ab67616d0000b273708ed519ab2ea26c8ba69a19")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            LinearGradient(gradient: .init(colors: [.blue,.black]), startPoint: .top, endPoint: .center)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                Spacer()
+                AsyncImage(url: url, content: { returnedImage in returnedImage.resizable().scaledToFill().frame(width: 250, height: 250)
+                }, placeholder: {
+                    ProgressView()
+                })
+                Text(musica.name).foregroundColor(Color.white).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text(musica.artist).foregroundColor(Color.white)
+                Spacer()
+                HStack{
+                    Image(systemName: "shuffle").resizable().frame(width: 30,height: 30)
+                        .foregroundColor(Color.white).padding()
+                        Image(systemName: "backward.end.fill").resizable().frame(width: 40,height: 40)
+                            .foregroundColor(Color.white).padding()
+                        Image(systemName: "play.fill").resizable().frame(width: 50,height: 50)
+                        .foregroundColor(Color.white).padding()
+                        Image(systemName: "forward.end.fill").resizable().frame(width: 40,height: 40)
+                            .foregroundColor(Color.white).padding()
+                        Image(systemName: "repeat").resizable().frame(width: 30,height: 30)
+                        .foregroundColor(Color.white).padding()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
